4132145a765dc5a16c94457c688fa91bfd0c35a7
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-01 23:52:23 +0900

    Complete 4.4

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+const totalLikes = (blogs) => {
+    return blogs.reduce(
+        (acc, cur) => acc + cur.likes, 0
+    )
+}
 
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy,
+    totalLikes
+}
