57f463849b0aec67fdc93f342d6a3e9dba7621ad
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-02 15:45:28 +0900

    Complete 4.5

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -3,11 +3,23 @@ const dummy = (blogs) => {
 }
 const totalLikes = (blogs) => {
     return blogs.reduce(
-        (acc, cur) => acc + cur.likes, 0
+        (acc, curr) => acc + curr.likes, 0
     )
+}
+
+const favoriteBlog = (blogs) => {
+    let maxLikesBlog = blogs.reduce((acc, curr) => {
+        return acc.likes > curr.likes ? acc : curr
+    })
+    return {
+        title: maxLikesBlog.title,
+        author: maxLikesBlog.author,
+        likes: maxLikesBlog.likes
+    }
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
