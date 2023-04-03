c3772a21adc83906448db3385f53667a8d46cdf7
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-03 23:48:59 +0900

    Complete 4.9

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -14,7 +14,12 @@ test('blogs are returned as json', async () => {
 test('there are three blogs', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(3)
-})
+}, 100000)
+
+test('UID of the blog posts is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+}, 100000)
 
 afterAll(async () => {
     await mongoose.connection.close()
