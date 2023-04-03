baa99b60d6ffb8e15a6ca5f128a13d69682ce2b4
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-04 00:29:23 +0900

    Complete 4.10

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+const promiseArray = blogObjects.map(blog => blog.save())
+await Promise.all(promiseArray)
+})
 
 test('blogs are returned as json', async () => {
     await api
@@ -19,6 +28,30 @@ test('there are three blogs', async () => {
 test('UID of the blog posts is named "id"', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
+}, 100000)
+
+test('A new blog can be added', async () => {
+    const newBlog = {
+        title: "A new blog",
+        author: "Mr Slut",
+        url: "www.wowee.org/yikes",
+        likes: 50
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const authors = response.body.map(r => r.author)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(authors).toContain(
+        'Mr Slut'
+    )
 }, 100000)
 
 afterAll(async () => {
---
+++ blog-list/tests/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: "Great reads",
+        author: "Mr Slut",
+        url: "www.wowee.org/books",
+        likes: 20
+    },
+    {
+        title: "Life hacks for rhinos",
+        author: "Rhianna Hippo",
+        url: "www.zoomimals.org/today",
+        likes: 30
+    },
+    {
+        title: "Chocolate teapots",
+        author: "Useless Mark",
+        url: "www.justwhy.org/what",
+        likes: 25
+    }
+]
+
+module.exports = {
+    initialBlogs
+  }
