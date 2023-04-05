eb17f097cf721cf6a2280782ad31e5b5937bdb9d
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-05 12:16:24 +0900

    Complete 4.15
    Implement way to create new users with a POST request and receive list of users with a GET request

--- blog-list/tests/blog_api.test.js
+++ blog-list/tests/blog_api.test.js
@@ -3,8 +3,10 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcryptjs')
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -136,6 +138,39 @@ test('information about a blog can be updated', async () => {
     expect(response.body.likes).toBe(blogToUpdate.likes + 1)
 
 }, 100000)
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
 
 afterAll(async () => {
     await mongoose.connection.close()
