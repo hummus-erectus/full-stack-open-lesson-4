a0402be120d09bec635977035921096559cca8f6
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-06 16:39:45 +0900

    Complete 4.17

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require ('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
@@ -16,7 +17,15 @@ blogsRouter.get('/:id', async (request, response) => {
   })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+
+    const user = await User.findById("642ce5476dfc2f775a33565a")
+    console.log(user)
+
+    const blog = new Blog({
+        ...request.body,
+        user: user
+    })
+
     if(!blog.title || !blog.url){
         response.status(400).end()
     } else {
@@ -24,6 +33,8 @@ blogsRouter.post('/', async (request, response) => {
             blog.likes = 0
         }
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(201).json(savedBlog)
     }
 })
