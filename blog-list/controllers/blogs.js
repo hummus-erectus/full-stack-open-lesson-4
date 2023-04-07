7daf7d03c244adaa2d71569d2177aac45b2f43d6
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-07 16:45:59 +0900

    Complete 4.19 / 4.20

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -1,4 +1,7 @@
 const blogsRouter = require ('express').Router()
+const jwt = require('jsonwebtoken')
+const config = require("../utils/config")
+const middleware = require("../utils/middleware")
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -16,27 +19,32 @@ blogsRouter.get('/:id', async (request, response) => {
     }
   })
 
-blogsRouter.post('/', async (request, response) => {
+  blogsRouter.post('/', async (request, response) => {
+    const decodedToken = jwt.verify(request.token, config.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
 
-    const user = await User.findById("642ce5476dfc2f775a33565a")
-    console.log(user)
+    const user = await User.findById(decodedToken.id)
+
+    if(!request.body.title || !request.body.url){
+        return response.status(400).end()
+    }
 
     const blog = new Blog({
         ...request.body,
-        user: user
+        user: user._id
     })
 
-    if(!blog.title || !blog.url){
-        response.status(400).end()
-    } else {
-        if(!blog.likes){
-            blog.likes = 0
-        }
-        const savedBlog = await blog.save()
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-        response.status(201).json(savedBlog)
-    }
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    // Use .populate() method to join the user information to the blog object
+    const populatedBlog = await Blog.findById(savedBlog._id).populate('user', { username: 1, name: 1 })
+
+    response.status(201).json(populatedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -54,11 +62,12 @@ blogsRouter.put('/:id', async (request, response) => {
         likes: body.likes
     }
 
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
         response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = blogsRouter
