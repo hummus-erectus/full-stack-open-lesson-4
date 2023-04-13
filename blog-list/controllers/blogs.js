2592a58acfbdf8cdf627273aad2fd35cafb1ebb0
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-13 16:36:05 +0900

    Complete 5.17

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -94,7 +94,7 @@ blogsRouter.put('/:id', async (request, response) => {
     }
 
     try {
-        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true }).populate('user', { username: 1, name: 1 })
         response.json(updatedBlog)
     } catch (error) {
         next(error)
